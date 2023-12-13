623c25037fc6a869561f52264ca7b3d82a7bd218
Andrzej Wroblewski <wroblewski.and@gmail.com>
2023-12-13 12:19:00 +0100

    przechwytywanie koordynatów z planszy, L8

--- statki.py
+++ statki.py
@@ -3,6 +3,7 @@ from kivy.app import App
 from kivy.uix.gridlayout import GridLayout
 
 import plane
+from gamebutton import GameButton
 
 class Statki(GridLayout):
 
@@ -10,12 +11,28 @@ class Statki(GridLayout):
         super(Statki, self).__init__(**kwargs)
         self.ids['przeciwnik'].disabled = True
 
+        for c1 in self.children:
+            for c2 in c1.children:
+                for c3 in c2.children:
+                    for c4 in c3.children:
+                        if isinstance(c4, GameButton):
+                            c4.sendMessage = self.sendMessage
+
     def startButtonClick(self):
         self.ids['gracz'].disabled = True
         self.ids['przeciwnik'].disabled = False
         self.ids['startGameButton'].disabled = True
         self.ids['gameId'].disabled = True
         print("Click")
+
+    def onMessage(self, message):
+        pass
+    
+    def sendMessage(self, message):
+        print(message)
+
+    def isShip(self, x, y):
+        return self.ids['player'].ids[y].ids[x].isShip
         
 class StatkiApp(App):
     
